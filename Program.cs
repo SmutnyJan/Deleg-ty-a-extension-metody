0f1bdf661bdb5c64d5ecddd6a73e15991fcde882
SmutnyJan <jan.smutny@pslib.cz>
2020-02-25 22:38:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Delegáty_a_extensionmetody;
+using System.Linq;
+
+namespace Delegáty_a_extensionmetody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> empty = new List<int>();
+            IList<string> colors = new List<string> 
+            { 
+                "red", "blue", "green", "magenta", "yellow", "cyan", "purple", "orange", "azure", "black", "white","gray","brown","gold", "silver", "red", "yellow", "purple", "green", "green", "red", "red"
+            };
+            Console.WriteLine(colors.RandomElement());
+            IList<string> mylist = new List<string>();
+            mylist = colors.AppearanceGreaterThen(2);
+            foreach(var i in mylist)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(mylist.RandomElementsWhere("e", 25));
+
+
+
+        }
+    }
+}
---
+++ TestExtension.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Delegáty_a_extensionmetody
+{
+    public static class TestExtensions
+    {
+        static Random rnd = new Random();
+        // RandomElement
+        public static T RandomElement<T>(this IList<T> list)
+        {
+            if (list.Count == 0)
+            {
+                throw new Exception("Pole má 0 prvků.");
+            }
+
+            else
+            {
+                int index = rnd.Next(list.Count);
+                return (list[index]);
+            }
+        }
+
+        // RandomElementsWhere
+        public static string RandomElementsWhere(this IList<string> list, string contains, int count)
+        {
+            IList<string> temp = new List<string>();
+
+            string str = "";
+            if (list.Count == 0)
+            {
+                throw new Exception("Pole má 0 prvků.");
+            }
+            else
+            {
+                foreach (var st in list.GroupBy(st => st, (key, group) => new { color = key, count = count }).Where(x => x.color.Contains(contains)).Take(count))
+                {
+                    str += st.color.ToString() + " ";
+                }
+            }
+            if (str.Length == 0) {
+                Exception e = new Exception(String.Format("Žádný prvek neobsahuje {0}", contains));
+
+                throw e;
+            }
+            return str;
+        }
+
+
+        // AppearanceGreaterThen
+        public static IList<string> AppearanceGreaterThen<T>(this IList<T> list, int param)
+        {
+            IList<string> temp = new List<string>();
+            if (list.Count == 0)
+            {
+                throw new Exception("Pole má 0 prvků.");
+            }
+            else
+            {
+                foreach (var st in list.GroupBy(s => s, (key, group) => new { color = key, count = group.Count() }).Where(x => x.count >= param))
+                {
+                    temp.Add(st.color.ToString());
+                }
+            }
+            if (temp.Count == 0)
+            {
+                Exception e = new Exception(String.Format("Žádný prvek není obsažen více jak {0}", param));
+
+                throw e;
+            }
+            else
+            {
+                return temp;
+            }
+        }
+    }
+}
